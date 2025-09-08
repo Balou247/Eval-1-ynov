17e6e5ab6d8c42e942adafe09b246898c9bc25ce
unknown <baptisteancelin76@gmail.com>
2025-09-08 16:41:28 +0200

    checknumber

--- checknumber.go
+++ checknumber.go
@@ -10,3 +10,12 @@ func CheckNumber(arg string) bool {
 	return False
 	}
 }
+
+import "github.com/01-edu/z01" 
+func CountAlpha(s string) int { 
+	var cpt int = 0 
+	for i := 0; i < len(s); i++ { 
+		cpt += 1 
+	}
+	return cpt 
+}
