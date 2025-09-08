34ad9f1a96b08ed557f69ea04d6b9c2b9cea36ca
unknown <baptisteancelin76@gmail.com>
2025-09-08 16:16:38 +0200

    printif

---
+++ printif.go
@@ -0,0 +1,12 @@
+package main
+
+import "github.com/01-edu/z01"
+
+func PrintIf(str string) string {
+	if len(s) < 4 {
+		z01.PrintRune(71)
+	}else { 
+		z01.PrintRune('Invalid Input')
+	}
+	z01.PrintRune('\n')
+}
