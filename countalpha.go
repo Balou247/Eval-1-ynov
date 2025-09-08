85ab9b4b95c331b09f0f49e92f420b98aed79c61
unknown <baptisteancelin76@gmail.com>
2025-09-08 16:45:12 +0200

    countalpha

--- countalpha.go
+++ countalpha.go
@@ -1,13 +1,11 @@
 package main
 
-import "unicode"
+import "github.com/01-edu/z01"
 
-func CountAlpha(s string) int {
-	cpt := 0
-	for _, r := range s {
-		if unicode.IsLetter(r) {
-			cpt++
-		}
+func CountAlpha(s string) int { 
+	var cpt int = 0 
+	for i := 0; i < len(s); i++ { 
+		cpt += 1 
 	}
-	return cpt
+	return cpt 
 }
