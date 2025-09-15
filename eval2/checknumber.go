7ca3be0532f37e9c362b960894bc393d123348a3
unknown <baptisteancelin76@gmail.com>
2025-09-15 15:28:21 +0200

    lvl 4

---
+++ eval2/checknumber.go
@@ -0,0 +1,12 @@
+package main
+
+import "github.com/01-edu/z01"
+
+func CheckNumber(txt string) bool {
+	for i := 0; i < len(txt); i++ {
+		if txt(i) = int {
+			return True
+		}
+	return False
+	}
+}
