2b581d7e20d063c03fac86ef0a7340b80def9190
unknown <baptisteancelin76@gmail.com>
2025-09-15 15:51:23 +0200

    lvl 5

--- eval2/repeatalpha.go
+++ eval2/repeatalpha.go
@@ -1,11 +1,21 @@
 package main
 
-import "github.com/01-edu/z01"
-
+import (
+	"github.com/01-edu/z01"
+	"fmt"
+	)
+	
 func RepeatAlpha(s string) string {
 	var phrase str = ""
 	for i := 0; i < ASCIINumber(s) - 97; i++ {
 		z01.PrintRune(s)
 	}
 	return str
+}
+
+func main() {
+	fmt.Println(piscine.RepeatAlpha("abc"))
+	fmt.Println(piscine.RepeatAlpha("Choumi."))
+	fmt.Println(piscine.RepeatAlpha(""))
+	fmt.Println(piscine.RepeatAlpha("abacadaba 01!"))
 }
